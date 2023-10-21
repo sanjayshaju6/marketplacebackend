e5db601db4fe793f418a05930d8cd8119d0f87d0
sanjayshaju6 <148597631+sanjayshaju6@users.noreply.github.com>
2023-10-20 20:58:01 -0400

    Add files via upload

---
+++ models/Product.js
@@ -0,0 +1,31 @@
+const mongoose = require ('mongoose');
+const { Schema } = mongoose;
+
+const Productschema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    quantity:{
+     type: Number,
+     required:true,
+    },
+    category:{
+     type: String,
+     required:true,
+    },
+    price:{
+        type: Number,
+        required: true,
+    },
+    Published:{
+        type :Boolean,
+        default:false
+    }
+  } ,{ timestamps: true });
+const Product = mongoose.model('Product',Productschema);
+module.exports = Product;
